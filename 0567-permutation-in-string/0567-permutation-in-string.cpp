89fb76705772d5aa92f00679f4eaa6f929057ace
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-06-24 04:51:37 +0530

    Time: 3 ms (65.63%), Space: 9.9 MB (71.02%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        int l=0, r=0;
+        vector<int> hash1(26,0), hash2(26,0);
+        for(int i=0;i<s1.size();i++) hash1[s1[i] - 'a']++;
+        while(r<s2.size()){
+            hash2[s2[r]-'a']++;
+            if(r-l+1 == s1.size()){
+                if(hash1 == hash2){
+                    return true;
+                }
+                else{
+                    hash2[s2[l]-'a']--;
+                    l++;
+                }
+            }
+            r++;
+        }
+        return false;
+    }
+};
