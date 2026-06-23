beaa9895eb0680275bc913d45433b3562278aa62
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-06-24 02:36:55 +0530

    Time: 39 ms (72.24%), Space: 84.9 MB (33.64%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -7,7 +7,8 @@ public:
         while(r < fruits.size()){
             mp[fruits[r]] = r;
             if(mp.size()>2){
-                auto it = min_element(mp.begin(),mp.end(),[](const auto &a, const auto &b) {return a.second < b.second;});
+                auto it = min_element(mp.begin(),mp.end(),
+                [](const auto &a, const auto &b) {return a.second < b.second;});
                 l = it->second + 1;
                 mp.erase(it);
             }
