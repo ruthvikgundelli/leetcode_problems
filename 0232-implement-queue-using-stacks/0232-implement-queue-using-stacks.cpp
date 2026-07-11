fff83b22a296ceffcb3bb3b3e0805e956056fae3
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-12 01:09:26 +0530

    Time: 0 ms (100%), Space: 10.1 MB (66.13%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -2,25 +2,29 @@ class MyQueue {
 public:
     stack<int> s1, s2;
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
     }
     int pop() {
-        int top = s1.top();
-        s1.pop();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int top = s2.top();
+        s2.pop();
         return top;
     }  
     int peek() {
-        return s1.top();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
     }
     bool empty() {
-        return s1.empty();
+        return s2.empty() && s1.empty();
     }
 };
