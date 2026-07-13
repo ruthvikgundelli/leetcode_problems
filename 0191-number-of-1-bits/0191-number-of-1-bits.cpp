1faf7f816bed45e5bf6ea09a4d98b42b0f82bcb0
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-14 02:57:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.18%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n);
+        int cnt = 0;
+        while(n != 0){
+            n = n&(n-1);
+            cnt++;
+        }
+        return cnt;
     }
 };
