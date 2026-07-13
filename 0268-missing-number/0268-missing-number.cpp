4479211df347bbe7775e1136c9014ec500e1984e
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-14 04:56:01 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.86%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum = 0;
-        for(auto x : nums){
-            sum += x;
+        // int sum = 0;
+        // for(auto x : nums){
+        //     sum += x;
+        // }
+        // long long total = (nums.size())*(nums.size()+1)/2;
+        // int ans = total - sum;
+        // return ans;
+        int n = nums.size();
+        int XOR = 0;
+        while(n){
+            XOR = XOR^n;
+            n--;
         }
-        long long total = (nums.size())*(nums.size()+1)/2;
-        int ans = total - sum;
-        return ans;
+        for(auto x : nums){
+            XOR = XOR^x;
+        }
+        return XOR;
     }
 };
