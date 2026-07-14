10519cbcdf93e1b2c2fab676c1e53985386e4448
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-14 19:54:55 +0530

    Time: 3 ms (27.79%), Space: 9.9 MB (60.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,14 +4,19 @@ public:
         int n = 1 << nums.size();
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
-            vector<int> temp;
-            int take = i, idx = 0;
-            while(take != 0){
-                if(take&1) temp.push_back(nums[idx]);
-                idx++;
-                take >>= 1;
+            if(i == 0) {
+                ans.push_back({});
             }
-            ans.push_back(temp);
+            else{
+                vector<int> temp;
+                int take = i, idx = 0;
+                while(take != 0){
+                    if(take&1) temp.push_back(nums[idx]);
+                    idx++;
+                    take >>= 1;
+                }
+                ans.push_back(temp);
+            }
         }
         return ans;
     }
