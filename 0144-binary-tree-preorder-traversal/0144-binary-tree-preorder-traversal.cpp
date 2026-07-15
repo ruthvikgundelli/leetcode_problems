c5fa71f7558a90252643d7af01b0a7551e206de0
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-16 04:24:31 +0530

    Time: 0 ms (100%), Space: 11 MB (42.94%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,16 +11,30 @@
  */
  class Solution {
 public:
-    vector<int> ans;
-    void preorder(TreeNode* root){
-        if(root == nullptr) return;
-        ans.push_back(root->val);
-        preorder(root->left);
-        preorder(root->right);
-    }
+    // vector<int> ans;
+    // void preorder(TreeNode* root){
+    //     if(root == nullptr) return;
+    //     ans.push_back(root->val);
+    //     preorder(root->left);
+    //     preorder(root->right);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     ans.clear();
+    //     preorder(root);
+    //     return ans;
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
-        ans.clear();
-        preorder(root);
+        if(root == nullptr) return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            if(node->right != nullptr) st.push(node->right);
+            if(node->left != nullptr) st.push(node->left);
+        }
         return ans;
     }
 };
