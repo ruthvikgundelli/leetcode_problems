491adadc251fd9c311d98d89b94968cad7c479bc
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-16 21:41:57 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.19%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr || q == nullptr){
+            if(p == q) return true;
+            else return false;
+        }
+        else if(p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
