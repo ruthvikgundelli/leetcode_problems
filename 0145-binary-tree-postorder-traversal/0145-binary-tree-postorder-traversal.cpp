62348db171c223da59f4163c5393a5553a8c5622
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-16 06:07:18 +0530

    Time: 0 ms (100%), Space: 11 MB (73.55%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,16 +11,43 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    void postorder(TreeNode* root){
-        if(root == nullptr) return;
-        postorder(root->left);
-        postorder(root->right);
-        ans.push_back(root->val);
-    }
+    // vector<int> ans;
+    // void postorder(TreeNode* root){
+    //     if(root == nullptr) return;
+    //     postorder(root->left);
+    //     postorder(root->right);
+    //     ans.push_back(root->val);
+    // }
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     ans.clear();
+    //     postorder(root);
+    //     return ans;
+    // }
     vector<int> postorderTraversal(TreeNode* root) {
-        ans.clear();
-        postorder(root);
+        if(root == nullptr) return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while(node != nullptr || !st.empty()){
+            if(node != nullptr){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                node = st.top();
+                if(node->right != nullptr) node = node->right;
+                else{
+                    st.pop();
+                    ans.push_back(node->val);
+                    while(!st.empty() && node == st.top()->right){
+                        node = st.top();
+                        st.pop();
+                        ans.push_back(node->val);
+                    }
+                    node = nullptr;
+                }
+            }
+        }
         return ans;
     }
 };
