ec2dbce86e24d189978f0e08de0b36b12993665f
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-16 19:13:17 +0530

    Time: 0 ms (100%), Space: 22.3 MB (7.12%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    // int maxDepth(TreeNode* root) {
+    //     if (root == nullptr) return 0;
+    //     return 1 + max(maxDepth(root->left),maxDepth(root->right)); 
+    // }
     int maxDepth(TreeNode* root) {
         if(root == nullptr) return 0;
         int depth = 0;
