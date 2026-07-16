0450a6d748122d0f0116584660df353c826c4472
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-07-16 18:45:36 +0530

    Time: 0 ms (100%), Space: 12.7 MB (26.56%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
+    // TreeNode* invertTree(TreeNode* root) {
+    //     if(root == nullptr) return nullptr;
+    //     stack<TreeNode*> st;
+    //     st.push(root);
+    //     while(!st.empty()){
+    //         TreeNode* node = st.top();
+    //         st.pop();
+    //         if(node->left != nullptr) st.push(node->left);
+    //         if(node->right != nullptr) st.push(node->right);
+    //         swap(node->left, node->right);
+    //     }
+    //     return root;
+    // }
     TreeNode* invertTree(TreeNode* root) {
         if(root == nullptr) return nullptr;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            TreeNode* node = st.top();
-            st.pop();
-            if(node->left != nullptr) st.push(node->left);
-            if(node->right != nullptr) st.push(node->right);
-            swap(node->left, node->right);
-        }
+        swap(root->left,root->right);
+        invertTree(root->left);
+        invertTree(root->right);
         return root;
     }
 };
