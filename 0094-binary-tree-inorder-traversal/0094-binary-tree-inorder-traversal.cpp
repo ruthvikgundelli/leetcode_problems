c983b0ae9239087151f15237f74e0a110f1e977c
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-05 09:31:40 +0530

    Time: 0 ms (100%), Space: 11 MB (12.34%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -7,26 +7,56 @@ public:
     //     ans.push_back(root->val);
     //     inorder(root->right);
     // }
-    // vector<int> inorderTraversal(TreeNode* root) {
+    // vector<int> inorderTraversal(TreeNode* root) { // Recursive
     //     ans.clear();
     //     inorder(root);
     //     return ans;
     // }
-    vector<int> inorderTraversal(TreeNode* root) {
+    // vector<int> inorderTraversal(TreeNode* root) { // Iterative
+    //     if(root == nullptr) return {};
+    //     vector<int> ans;
+    //     stack<TreeNode*> st;
+    //     TreeNode* node = root;
+    //     while(node != nullptr || !st.empty()){
+    //         if(node != nullptr){
+    //             st.push(node);
+    //             node = node->left;
+    //         }
+    //         else{
+    //             node = st.top();
+    //             st.pop();
+    //             ans.push_back(node->val);
+    //             node = node->right;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    vector<int> inorderTraversal(TreeNode* root) { // Morris Traversal
         if(root == nullptr) return {};
         vector<int> ans;
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-        while(node != nullptr || !st.empty()){
-            if(node != nullptr){
-                st.push(node);
-                node = node->left;
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->left){
+                TreeNode* node = curr->left;
+                bool check = true;
+                while(node->right){
+                    if(node->right == curr){
+                        node->right = nullptr;
+                        ans.push_back(curr->val);
+                        curr = curr->right;
+                        check = false;
+                        break;
+                    }
+                    node = node->right;
+                }
+                if(check){
+                    node->right = curr;
+                    curr = curr->left;
+                }
             }
             else{
-                node = st.top();
-                st.pop();
-                ans.push_back(node->val);
-                node = node->right;
+                ans.push_back(curr->val);
+                curr = curr->right;
             }
         }
         return ans;
