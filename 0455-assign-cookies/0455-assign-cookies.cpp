3f3f5756f77bfc28e752175f8ad1d590d0a771a0
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-07 00:58:18 +0530

    Time: 15 ms (14.93%), Space: 44.9 MB (12.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,11 +5,8 @@ public:
         sort(s.begin(),s.end());
         int i = 0, j = 0;
         while(i < g.size() && j < s.size()){
-            if(s[j] >= g[i]){
-                j++;
-                i++;
-            }
-            else j++;
+            if(s[j] >= g[i]) i++;
+            j++;
         }
         return i;
     }
