d074d3de2cdaf2e76318c01f6d84c8c4cdd737a6
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-12 00:29:10 +0530

    Time: 0 ms (100%), Space: 112.4 MB (88.89%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas = 0, totalCost = 0;
+        int ans = 0;
+        int fuel = 0;
+        for(int i = 0; i < gas.size(); i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+            fuel += gas[i] - cost[i];
+            if(fuel < 0){
+                ans = i + 1;
+                fuel = 0;
+            }
+        }
+        if(totalGas < totalCost) return -1;
+        return ans;
+    }
+};
