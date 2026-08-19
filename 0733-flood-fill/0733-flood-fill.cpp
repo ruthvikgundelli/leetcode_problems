f4ca690e38712233f81db63f645c03e3048fddc1
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-20 01:33:37 +0530

    Time: 0 ms (100%), Space: 18 MB (75.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void dfs(int sr, int sc, int prev, int color, vector<vector<int>>& vis, vector<vector<int>>& image){
-        vis[sr][sc] = 1;
+    void dfs(int sr, int sc, int prev, int color, vector<vector<int>>& image){
         image[sr][sc] = color;
-        if(sr-1 >= 0 && !vis[sr-1][sc] && image[sr-1][sc] == prev){
-            dfs(sr-1, sc, prev, color, vis, image);
+        if(sr-1 >= 0 && image[sr-1][sc] == prev){
+            dfs(sr-1, sc, prev, color, image);
         }
-        if(sr+1 < image.size() && !vis[sr+1][sc] && image[sr+1][sc] == prev){
-            dfs(sr+1, sc, prev, color, vis, image);
+        if(sr+1 < image.size() && image[sr+1][sc] == prev){
+            dfs(sr+1, sc, prev, color, image);
         }
-        if(sc-1 >= 0 && !vis[sr][sc-1] && image[sr][sc-1] == prev){
-            dfs(sr, sc-1, prev, color, vis, image);
+        if(sc-1 >= 0 && image[sr][sc-1] == prev){
+            dfs(sr, sc-1, prev, color, image);
         }
-        if(sc+1 < image[0].size() && !vis[sr][sc+1] && image[sr][sc+1] == prev){
-            dfs(sr, sc+1, prev, color, vis, image);
+        if(sc+1 < image[0].size() && image[sr][sc+1] == prev){
+            dfs(sr, sc+1, prev, color, image);
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size();
-        int n = image[0].size();
-        vector<vector<int>> vis(m, vector<int> (n, 0));
         int prev = image[sr][sc];
-        dfs(sr, sc, prev, color, vis, image);
+        if(prev == color) return image;
+        dfs(sr, sc, prev, color, image);
         return image;
     }
 };
