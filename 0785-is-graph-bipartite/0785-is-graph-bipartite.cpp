97974e413d91736db75e4758eb3977b7f3d118e1
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-23 22:36:51 +0530

    Time: 0 ms (100%), Space: 17.3 MB (79.25%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,28 +1,50 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int n = graph.size();
+    //     vector<int> color(n, -1);
+    //     queue<int> q;
+    //     for(int i = 0; i < n; i++){
+    //         if(color[i] != -1) continue;
+    //         q.push(i);
+    //         color[i] = 0;
+    //         while(!q.empty()){
+    //             int node = q.front();
+    //             q.pop();
+    //             for(auto ad : graph[node]){
+    //                 if(color[ad] == color[node]){
+    //                     return false;
+    //                 }
+    //                 else if(color[ad] == -1){
+    //                     if(color[node] == 0) color[ad] = 1;
+    //                     else color[ad] = 0;
+    //                     q.push(ad);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& graph){
+        color[node] = col;
+        for(auto ad : graph[node]){
+            if(color[ad] == -1){
+                if(!dfs(ad, !col, color, graph)) return false;
+            }
+            else if(color[ad] == col) return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph){
         int n = graph.size();
+        
         vector<int> color(n, -1);
-        queue<int> q;
         for(int i = 0; i < n; i++){
-            if(color[i] != -1) continue;
-            q.push(i);
-            color[i] = 0;
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                for(auto ad : graph[node]){
-                    if(color[ad] == color[node]){
-                        return false;
-                    }
-                    else if(color[ad] == -1){
-                        if(color[node] == 0) color[ad] = 1;
-                        else color[ad] = 0;
-                        q.push(ad);
-                    }
-                }
+            if(color[i] == -1){
+                if(!dfs(i, 0, color, graph)) return false;
             }
         }
+
         return true;
     }
 };
