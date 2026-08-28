04998a55fe809bd7e0479a2dbedc39b64dccd48d
Ruthvik Gundelli <ruthvikgundelli@gmail.com>
2026-08-28 13:43:43 +0530

    Time: 63 ms (46.45%), Space: 19.9 MB (87.08%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,62 +1,32 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int check = 0;
-        for(auto word : wordList){
-            if(word == endWord) check++;
-        }
+        
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        if(!st.count(endWord)) return 0;
 
-        if(!check) return 0;
-
-        int n = beginWord.size();
-        int m = wordList.size();
-
-        vector<string> words;
-        words.push_back(beginWord);
-
-        for(auto word : wordList){
-            words.push_back(word);
-        }
-
-        vector<vector<int>> adj(m+1);
-        vector<int> vis(m+1, 0);
-
-        for(int i = 0; i < m + 1; i++){
-            for(int j = i + 1; j < m + 1; j++){
-                int cnt = 0;
-
-                for(int k = 0; k < n; k++){
-                    if(words[i][k] != words[j][k]){
-                        cnt++;
-                    }
-                }
-
-                if(cnt == 1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-
-        vector<int> dist(m + 1, INT_MAX);
-        queue<pair<int,int>> q;
-        q.push({0, 1});
-        dist[0] = 1;
-
+        queue<string> q;
+        q.push(beginWord);
+        int ans = 1;
         while(!q.empty()){
-            auto [node, dis] = q.front();
-            q.pop();
-
-            if(words[node] == endWord){
-                return dis;
-            }
-
-            for(auto ad : adj[node]){
-                if(dist[ad] == INT_MAX){
-                    dist[ad] = dis + 1;
-                    q.push({ad, dist[ad]});
+            int size = q.size();
+            while(size--){
+                string node = q.front();
+                q.pop();
+                if(node == endWord) return ans;
+                string temp = node;
+                for(int i = 0; i < node.size(); i++){
+                    for(int j = 0; j < 26; j++){
+                        node[i] = 'a' + j;
+                        if(node != temp && st.count(node)){
+                            q.push(node);
+                            st.erase(node);
+                        }
+                    }
+                    node[i] = temp[i];
                 }
             }
+            ans++;
         }
         return 0;
     }
